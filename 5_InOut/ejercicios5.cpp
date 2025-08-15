14275f05c86949262e0a73e71e5ffaba5bc9bf3e
dogo <dievilchez23@gmail.com>
2025-08-15 13:40:05 +0000

    Add input&output excercises

---
+++ 5_InOut/ejercicios5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+using std::cout;
+main () {
+    int respuesta;
+    cout << "Cual es tu num favorito:";
+    cin >> respuesta;
+    cout << "Ese es mi num favorito" << endl;
+    cout << "enserio?? " << respuesta << " es mi numero favorito" << endl;
+    return 0;
+}
