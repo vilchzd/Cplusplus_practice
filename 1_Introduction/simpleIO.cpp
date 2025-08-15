9f56a9a2f52013a1504684ea623111d5da66fda6
dogo <dievilchez23@gmail.com>
2025-08-15 13:29:31 +0000

    Add introductory exercise

---
+++ 1_Introduction/simpleIO.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main () {
+    int respuesta;
+    std::cout << "Ingresa tu edad:";
+    std::cin >> respuesta;
+    std::cout << "Esa tambien es mi edad" << std::endl;
+    std::cout << "enserio?? " << respuesta << " es mi edad favorita lmao" << std::endl;
+    return 0;
+}
